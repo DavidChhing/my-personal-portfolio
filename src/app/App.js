357687e66d34196d48cbb007ac118bf269f6e35d
Davidchhing <davidhchhing@gmail.com>
2019-03-26 15:54:30 -0700

    Added estlingignore and estlintrc and attempting to style app.

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ROUTES } from '../routes';
+import styles from './App.scss';
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route
-          exact
-          path={ROUTES.HOME.path}
-          component={ROUTES.HOME.Component}
-        />
-      </Switch>
-    </Router>
+    <main className={styles.App}>
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path={ROUTES.HOME.path}
+            component={ROUTES.HOME.Component}
+          />
+        </Switch>
+      </Router>
+    </main>
   );
 }
