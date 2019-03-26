f35b12debd2a679cbdea0e0e0946cac23b95ccf5
Davidchhing <davidhchhing@gmail.com>
2019-03-26 16:34:18 -0700

    Fixed package json issue.

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,54 @@ module.exports = {
     filename: 'bundle.[hash].js',
     publicPath: '/',
   },
-  mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.js$/,
         loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|ico)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 5000 },
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'image/svg+xml',
+          },
+        },
       },
     ],
   },
