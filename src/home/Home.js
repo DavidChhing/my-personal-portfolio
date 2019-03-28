5486fa6f8490e965bb81455e6bbcc9485932ebcc
Davidchhing <davidhchhing@gmail.com>
2019-03-28 13:02:32 -0700

    Added react icons.

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import styles from './Home.scss';
+import React, { Component } from 'react';
 
-export default function Home() {
-  return (
-    <main className={styles.Home}>
-      <h1>
-        David
-        <span className="secondary-text">Chhing</span>
-      </h1>
-    </main>
-  );
+import styles from './Home.scss';
+import { MdMenu as Menu } from 'react-icons/md';
+import { FaLinkedin as LinkedIn, FaGithub as Github } from 'react-icons/fa';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <main className={styles.Home}>
+        <button>
+          <Menu />
+        </button>
+        <h1 className="name">
+          David
+          <span className="secondary-text">Chhing</span>
+        </h1>
+        <button>
+          <LinkedIn />
+          <Github />
+        </button>
+        <footer className="footer-name-copyright">
+          Handcrafted by David Chhing 2019 &copy;
+        </footer>
+      </main>
+    );
+  }
 }
